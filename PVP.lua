c24a55a01e66373ea42be803a84a2b19704df9cb
ElDevBorges <eldevborges@gmail.com>
2025-02-27 21:30:18 -0300

    PVP

---
+++ PVP.lua
@@ -0,0 +1,132 @@
+
+bit32={};local v0=5 + (82 -55) ;local v1=(1 + 1)^v0 ;bit32.bnot=function(v18) local v19=0;local v20;while true do if (v19==(0 + 0)) then v20=0 -0 ;while true do if (v20==(0 -0)) then v18=v18%v1 ;return (v1-1) -v18 ;end end break;end end end;bit32.band=function(v21,v22) local v23=0 -0 ;local v24;local v25;while true do if (v23==0) then if (v22==((78 + 1362) -(979 + 206))) then return v21%((161 + 293) -(1083 -(261 + 624))) ;end if (v22==65535) then return v21%((115532 -50520) + (1604 -(1020 + 60))) ;end v23=1424 -(630 + 793) ;end if (v23==3) then for v105=1,v0 do local v106=0;local v107;local v108;local v109;while true do if (v106==1) then v109=nil;while true do if (v107==(0 -0)) then v108,v109=v21%((2102 -1658) -(416 + 26)) ,v22%((3 + 3) -4) ;v21,v22=math.floor(v21/(1 + (3 -2)) ),math.floor(v22/(3 -(1748 -(760 + 987))) );v107=439 -((2058 -(1789 + 124)) + 293) ;end if (v107==(431 -(44 + (1152 -(745 + 21))))) then if ((v108 + v109)==((512 + 976) -(998 + 488))) then v24=v24 + v25 ;end v25=(1 + (2 -1)) * v25 ;break;end end break;end if (v106==(0 -0)) then v107=(0 + 0) -0 ;v108=nil;v106=1 + 0 ;end end end return v24;end if (v23==(1057 -(87 + 968))) then v24=(0 -0) -(0 + 0) ;v25=(42 -23) -(10 + (1421 -(447 + 966))) ;v23=3;end if (v23==(2 -1)) then if (v22==((4294969903 -(1703 + 114)) -(368 + (1124 -(376 + 325))))) then return v21%(4294967296 -0) ;end v21,v22=v21%v1 ,v22%v1 ;v23=5 -3 ;end end end;bit32.bor=function(v26,v27) local v28=0 + 0 ;local v29;local v30;local v31;while true do if (v28==(0 -0)) then v29=(14 -(9 + 5)) + (376 -(85 + 291)) ;v30=nil;v28=1;end if (v28==(1266 -(243 + 1022))) then v31=nil;while true do if (v29==(7 -5)) then local v114=0 + 0 ;while true do if ((1180 -(1123 + 57))==v114) then v30=772 -(164 + 37 + (825 -(163 + 91))) ;v31=1;v114=1;end if (v114==1) then v29=(3071 -(1869 + 61)) -(33 + 83 + 1022) ;break;end end end if (v29==0) then if (v27==(1061 -806)) then return (v26-(v26%((531 -380) + 105))) + ((1428 -498) -(93 + 582)) ;end if (v27==((319814 -87094) -(157033 + 10152))) then return (v26-(v26%(67010 -(1329 + 145)))) + ((67365 -(140 + 831)) -((2664 -(1409 + 441)) + 45)) ;end v29=719 -(15 + 703) ;end if (v29==((4 + 3) -(442 -(262 + 176)))) then for v120=(1722 -(345 + 1376)) + (688 -(198 + 490)) ,v0 do local v121,v122=v26%(1 + (4 -3)) ,v27%((2127 -1240) -((1467 -(696 + 510)) + (1308 -684))) ;v26,v27=math.floor(v26/((1265 -(1091 + 171)) -(1 + 0)) ),math.floor(v27/(1082 -(1020 + (188 -128))) );if ((v121 + v122)>=(1424 -((2089 -1459) + (1167 -(123 + 251))))) then v30=v30 + v31 ;end v31=((29 -23) -(702 -(208 + 490))) * v31 ;end return v30;end if ((1 + 0)==v29) then if (v27==(4294967295 -(0 + 0))) then return 4294967295;end v26,v27=v26%v1 ,v27%v1 ;v29=838 -(660 + 176) ;end end break;end end end;bit32.bxor=function(v32,v33) v32,v33=v32%v1 ,v33%v1 ;local v34=0 + 0 + 0 ;local v35=(205 -(14 + 188)) -(677 -(534 + 141)) ;for v88=1,v0 do local v89=0 + 0 ;local v90;local v91;local v92;while true do if (v89==(0 + 0)) then v90=1747 -(760 + 949 + 38) ;v91=nil;v89=1 -0 ;end if ((1 -0)==v89) then v92=nil;while true do if (((5368 -3454) -(1789 + 67 + 57))==v90) then if ((v91 + v92)==(1 + 0)) then v34=v34 + v35 ;end v35=((1164 -(115 + 281)) -(745 + (48 -27))) * v35 ;break;end if (v90==(0 + 0 + (0 -0))) then local v123=0 -0 ;while true do if (v123==(868 -(550 + 317))) then v90=1;break;end if (v123==(0 -0)) then v91,v92=v32%((7 -2) -(8 -5)) ,v33%((292 -(134 + 151)) -(1670 -(970 + 695))) ;v32,v33=math.floor(v32/(3 -1) ),math.floor(v33/(1992 -(582 + 1408)) );v123=3 -2 ;end end end end break;end end end return v34;end;bit32.lshift=function(v36,v37) local v38=0 -0 ;local v39;while true do if (v38==(0 -0)) then v39=(1824 -(1195 + 629)) + (0 -0) ;while true do if (v39==(1 + (241 -(187 + 54)))) then if (v37<(780 -(162 + 618))) then return math.floor(v36 * (((741 + 316) -(58 + 29 + (2064 -1096)))^v37) );else return (v36 * ((8 -(9 -3))^v37))%v1 ;end break;end if (v39==(0 + 0)) then local v115=0;while true do if (v115==(0 + 0)) then if (math.abs(v37)>=v0) then return 1636 -(1373 + 263) ;end v36=v36%v1 ;v115=1001 -(451 + 549) ;end if (v115==1) then v39=(1 + 1) -1 ;break;end end end end break;end end end;bit32.rshift=function(v40,v41) local v42=1413 -(447 + 966) ;while true do if (v42==1) then if (v41>0) then return math.floor(v40 * (((7 -2) -(4 -1))^ -v41) );else return (v40 * (((3203 -(746 + 638)) -(641 + 1062 + (172 -58)))^ -v41))%v1 ;end break;end if (((1042 -(218 + 123)) -(376 + 325))==v42) then if (math.abs(v41)>=v0) then return 1581 -(1535 + 46) ;end v40=v40%v1 ;v42=(1 + 0) -0 ;end end end;bit32.arshift=function(v43,v44) local v45=0;local v46;while true do if (v45==0) then v46=0 -(0 + 0) ;while true do if (v46==0) then if (math.abs(v44)>=v0) then return 560 -(306 + 254) ;end v43=v43%v1 ;v46=1;end if (v46==(1 + 0 + (0 -0))) then if (v44>((1467 -(899 + 568)) -0)) then local v126=(10 + 4) -(9 + 5) ;local v127;while true do if (v126==(0 -0)) then v127=603 -(268 + 335) ;if (v43>=(v1/((668 -(60 + 230)) -(85 + 291)))) then v127=v1-(((1839 -(426 + 146)) -(30 + 213 + (2478 -(282 + 1174))))^(v0-v44)) ;end v126=812 -(569 + 242) ;end if (v126==1) then return math.floor(v43 * ((5 -3)^ -v44) ) + v127 ;end end else return (v43 * ((7 -5)^ -v44))%v1 ;end break;end end break;end end end;xtela10nui,ytela10nui=38 + 614 + 138 ,1324 -(706 + 318) ;timemagianui="";tempoi1nui=121180 -((2374 -(721 + 530)) + 57) ;tempoi2nui=1271 -(945 + 326) ;texto10nui="NUIBARI";local v9=setupUI([[
+Panel
+  height: 400
+  width: 900
+]],g_ui.getRootWidget());local v10=g_ui.loadUIFromString([[
+Label
+  color: white
+  background-color: black
+  opacity: 0.85
+  text-horizontal-auto-resize: true
+]],v9);macro(2 -1 ,function() if ( not storage.time.nui1 or (storage.time.nui1<now)) then local v97=0 + 0 ;while true do if (v97==(700 -(271 + 429))) then v10:setText(texto10nui);v10:setColor("green");break;end end elseif (storage.time.nu1>=now) then local v110=0 + 0 ;local v111;while true do if (v110==(1500 -(1408 + 92))) then v111=(1086 -(461 + 625)) + 0 ;while true do if (v111==((1542 -(993 + 295)) -(163 + 91))) then v10:setColor("blue");v10:setText(texto10nui   .. string.format("%.0f",(storage.time.nu1-now)/(52 + 948) )   .. "s " );break;end end break;end end else local v112=0;while true do if (v112==(1171 -(418 + 753))) then v10:setColor("red");v10:setText(texto10nui   .. string.format("%.0f",(storage.time.nui1-now)/((1116 + 1814) -(193 + 1676 + 18 + 43)) )   .. "s " );break;end end end end);onTextMessage(function(v47,v48) if v48:find("para usar a espada novamente.") then local v98=0 + 0 ;local v99;while true do if (v98==(529 -(406 + 123))) then v99=1769 -(1749 + 20) ;while true do if (v99==(1 + 0)) then storage.time.nu1=now + tempoi2nui ;break;end if (v99==(0 + 0)) then local v129=1322 -(1249 + 73) ;while true do if (v129==0) then storage.time=storage.time or {} ;storage.time.nui1=now + tempoi1nui ;v129=1 + 0 ;end if (v129==(1146 -(466 + 679))) then v99=3 -(4 -2) ;break;end end end end break;end end end end);if ((type(storage.time)~="table") or  not storage.time.nu1 or ((storage.time.nui1-now)>(171611 -111611)) or  not storage.time.nu1 or ((storage.time.nu1-now)>(92157 -(34057 -(106 + 1794))))) then storage.time={nui1=0,nu1=0 + 0 + 0 + 0 };end v10:setPosition({y=ytela10nui + (54 -(41 -27)) ,x=xtela10nui + (27 -17) + (114 -(4 + 110)) });setDefaultTab("PVP");local v11="killSteal";local v12=setupUI([[
+Panel
+  height: 50
+
+  BotItem
+    id: item
+    anchors.top: parent.top
+    anchors.left: parent.left
+    margin-top: 2
+
+  BotSwitch
+    id: title
+    anchors.top: parent.top
+    anchors.left: item.right
+    anchors.bottom: item.verticalCenter
+    text-align: center
+    !text: tr('Kill Steal')
+    margin-left: 2
+    width: 90
+
+
+  BotLabel
+    id: help
+    anchors.top: item.verticalCenter
+    anchors.left: item.right
+    anchors.right: parent.right
+    anchors.bottom: item.bottom
+    text-align: center
+    margin-left: 2
+
+  HorizontalScrollBar
+    id: HP
+    anchors.top: item.bottom
+    anchors.left: parent.left
+    anchors.right: parent.right
+    margin-top: 3
+    minimum: 1
+    maximum: 100
+    step: 1
+
+]],parent);v12:setId(v11);if  not storage[v11] then storage[v11]={id=(5213 -(57 + 527)) -(1329 + (1572 -(41 + 1386))) ,title=enabled,enabled=false,setting=true,hp=123 -(17 + 86) };end v12.title:setOn(storage[v11].enabled);v12.title.onClick=function(v49) local v50=0;while true do if ((0 + 0)==v50) then storage[v11].enabled= not storage[v11].enabled;v49:setOn(storage[v11].enabled);break;end end end;local v14=function() local v51=0 -0 ;local v52;while true do if (v51==(0 -0)) then v52=nil;if storage[v11].setting then v52="Target";else v52="Enemy";end v51=167 -(122 + 44) ;end if (v51==(1 -0)) then v12.help:setText("If "   .. v52   .. " HP<"   .. storage[v11].hp   .. "%" );break;end end end;v14();v12.HP.onValueChange=function(v53,v54) local v55=(3221 -2250) -(114 + 26 + 121 + 710) ;while true do if (v55==((3747 -1897) -((1474 -(30 + 35)) + 304 + 137))) then storage[v11].hp=v54;v14();break;end end end;v12.item:setItemId(storage[v11].id);v12.item.onItemChange=function(v56) storage[v11].id=v56:getItemId();end;v12.HP:setValue(storage[v11].hp);macro((2175 -(1043 + 214)) -((56 -41) + 703) ,function() local v58=(1212 -(323 + 889)) + 0 ;local v59;while true do if (v58==0) then local v102=0 -0 ;while true do if (v102==(581 -(361 + 219))) then v58=439 -((582 -(53 + 267)) + 176) ;break;end if (v102==0) then if  not storage[v11].enabled then return;end v59=g_game.getLocalPlayer();v102=1 + 0 ;end end end if (v58==(1722 -(345 + (1789 -(15 + 398))))) then if storage[v11].setting then if (g_game.isAttacking() and g_game.getAttackingCreature():isPlayer() and (g_game.getAttackingCreature():getHealthPercent()<=storage[v11].hp)) then if ( not storage.time.nui1 or (storage.time.nui1<now)) then local v136=(1670 -(18 + 964)) -(198 + (1844 -1354)) ;local v137;local v138;local v139;local v140;local v141;while true do if (v136==(0 + 0)) then local v147=0;while true do if (1==v147) then v136=4 -3 ;break;end if (v147==(0 + 0)) then v137=g_game.getAttackingCreature();v138=v59:getPosition();v147=1;end end end if (v136==((854 -(20 + 830)) -2)) then v141=math.abs(v138.y-v139.y );if (((v140==((943 + 264) -((822 -(116 + 10)) + 38 + 472))) and (v141==(0 -0))) or ((v140==(1262 -((1829 -(542 + 196)) + (366 -195)))) and (v141==(1 + 0 + 0 + 0))) or ((v140==((2 + 1) -(4 -2))) and (v141==((7 -4) -(1553 -(1126 + 425)))))) then useWith(storage[v11].id,v137);end break;end if (((780 -(118 + 287)) -((481 -358) + (1372 -(118 + 1003))))==v136) then local v148=0 -0 ;while true do if ((377 -(142 + 235))==v148) then v139=v137:getPosition();v140=math.abs(v138.x-v139.x );v148=1;end if (v148==(4 -3)) then v136=(2 + 7) -(984 -(553 + 424)) ;break;end end end end end end else for v116,v117 in pairs(getSpectators()) do if (g_game.isAttacking() and (g_game.getAttackingCreature():getHealthPercent()<=storage[v11].hp)) then local v130=0;local v131;local v132;local v133;local v134;local v135;while true do if (v130==(3 -1)) then v135=nil;while true do if (v131==(1 + 0 + 1 + 0)) then if (((v134==(1 + 0 + 0 + 0)) and (v135==((478 + 358) -((1430 -770) + 176)))) or ((v134==((0 -0) + (0 -0))) and (v135==((60 + 143) -((67 -53) + 188)))) or ((v134==((1429 -(239 + 514)) -(188 + 346 + (1470 -(797 + 532))))) and (v135==(1 + 0 + 0)))) then useWith(storage[v11].id,v117);end break;end if (v131==(0 + 0 + (0 -0))) then v132=v117:getPosition();v133=v59:getPosition();v131=1203 -(373 + 829) ;end if (v131==(732 -(476 + 255))) then local v149=1130 -(369 + 761) ;while true do if (v149==1) then v131=2 + 0 ;break;end if (v149==(0 -0)) then v134=math.abs(v133.x-v132.x );v135=math.abs(v133.y-v132.y );v149=1 -0 ;end end end end break;end if (v130==(239 -(64 + 174))) then v133=nil;v134=nil;v130=2;end if (v130==(0 + 0)) then v131=698 -((307 -99) + 490) ;v132=nil;v130=1;end end end end end break;end end end);addSeparator();local v11="Tsukuyomi";local v12=setupUI([[
+Panel
+  height: 50
+
+  BotItem
+    id: item
+    anchors.top: parent.top
+    anchors.left: parent.left
+    margin-top: 2
+
+  BotSwitch
+    id: title
+    anchors.top: parent.top
+    anchors.left: item.right
+    anchors.bottom: item.verticalCenter
+    text-align: center
+    !text: tr('SELAR')
+    margin-left: 2
+    width: 90
+
+
+  BotLabel
+    id: help
+    anchors.top: item.verticalCenter
+    anchors.left: item.right
+    anchors.right: parent.right
+    anchors.bottom: item.bottom
+    text-align: center
+    margin-left: 2
+
+  HorizontalScrollBar
+    id: HP
+    anchors.top: item.bottom
+    anchors.left: parent.left
+    anchors.right: parent.right
+    margin-top: 3
+    minimum: 1
+    maximum: 100
+    step: 1
+
+]],parent);v12:setId(v11);if  not storage[v11] then storage[v11]={id=3491 -(144 + 192) ,title=enabled,enabled=false,setting=true,hp=(236 -(42 + 174)) + 0 + 0 };end v12.title:setOn(storage[v11].enabled);v12.title.onClick=function(v60) local v61=0 + 0 ;local v62;while true do if (v61==(0 + 0)) then v62=1504 -(363 + 1141) ;while true do if (v62==((1580 -(1183 + 397)) -0)) then storage[v11].enabled= not storage[v11].enabled;v60:setOn(storage[v11].enabled);break;end end break;end end end;local v14=function() local v63;if storage[v11].setting then v63="Target";else v63="Enemy";end v12.help:setText("If "   .. v63   .. " HP<"   .. storage[v11].hp   .. "%" );end;v14();v12.HP.onValueChange=function(v64,v65) local v66=0 -0 ;local v67;while true do if (v66==(0 + 0)) then v67=(0 + 0) -(1975 -(1913 + 62)) ;while true do if (v67==((0 + 0) -0)) then storage[v11].hp=v65;v14();break;end end break;end end end;v12.item:setItemId(storage[v11].id);v12.item.onItemChange=function(v68) storage[v11].id=v68:getItemId();end;v12.HP:setValue(storage[v11].hp);macro(1 + 0 ,function() local v70=0 -0 ;while true do if (v70==0) then if  not storage[v11].enabled then return;end if storage[v11].setting then if (g_game.isAttacking() and (g_game.getAttackingCreature():getHealthPercent()<storage[v11].hp)) then local v124=1933 -(565 + 1368) ;while true do if (((0 -0) + (1661 -(1477 + 184)))==v124) then tyrBot.storage.comboDelay=os.time() + (1 -0) ;say("Kekkai Shihou Fuujin");break;end end end end break;end end end);local v11="Tsukuyomi";local v12=setupUI([[
+Panel
+  height: 50
+
+  BotItem
+    id: item
+    anchors.top: parent.top
+    anchors.left: parent.left
+    margin-top: 2
+
+  BotSwitch
+    id: title
+    anchors.top: parent.top
+    anchors.left: item.right
+    anchors.bottom: item.verticalCenter
+    text-align: center
+    !text: tr('Tsukuyomi')
+    margin-left: 2
+    width: 90
+
+
+  BotLabel
+    id: help
+    anchors.top: item.verticalCenter
+    anchors.left: item.right
+    anchors.right: parent.right
+    anchors.bottom: item.bottom
+    text-align: center
+    margin-left: 2
+
+  HorizontalScrollBar
+    id: HP
+    anchors.top: item.bottom
+    anchors.left: parent.left
+    anchors.right: parent.right
+    margin-top: 3
+    minimum: 1
+    maximum: 100
+    step: 1
+
+]],parent);v12:setId(v11);if  not storage[v11] then storage[v11]={id=3551 -(108 + 7 + (1137 -(564 + 292))) ,title=enabled,enabled=false,setting=true,hp=20};end v12.title:setOn(storage[v11].enabled);v12.title.onClick=function(v71) local v72=0 -0 ;while true do if (v72==(0 -0)) then storage[v11].enabled= not storage[v11].enabled;v71:setOn(storage[v11].enabled);break;end end end;local v14=function() local v73;if storage[v11].setting then v73="Target";else v73="Enemy";end v12.help:setText("If "   .. v73   .. " HP<"   .. storage[v11].hp   .. "%" );end;v14();v12.HP.onValueChange=function(v74,v75) local v76=0 -(304 -(244 + 60)) ;while true do if ((0 + 0 + (476 -(41 + 435)))==v76) then storage[v11].hp=v75;v14();break;end end end;v12.item:setItemId(storage[v11].id);v12.item.onItemChange=function(v77) storage[v11].id=v77:getItemId();end;v12.HP:setValue(storage[v11].hp);macro((1003 -(938 + 63)) -(1 + 0) ,function() local v79=0;local v80;while true do if (v79==0) then v80=(1125 -(936 + 189)) -(0 + 0) ;while true do if (v80==(867 -(550 + 317))) then if  not storage[v11].enabled then return;end if storage[v11].setting then if (g_game.isAttacking() and (g_game.getAttackingCreature():getHealthPercent()<storage[v11].hp)) then say("tsukuyomi no jutsu");end end break;end end break;end end end);UI.Separator(hpPanel2);Panels.HealthItem(hpPanel2);Panels.HealthItem(hpPanel2);UI.Separator(hpPanel2);Panels.ManaItem(hpPanel2);Panels.ManaItem(hpPanel2);addSeparator();local v17=macro((1901 -(1565 + 48)) -(55 + 33) ,"Quebra-trap",function() local v81=0;local v82;local v83;local v84;local v85;local v86;local v87;while true do if (v81==(1140 -(782 + 356))) then if (v84==((267 -(176 + 91)) -0)) then v85.y=v85.y-(2 -(2 -1)) ;elseif (v84==(1 -0)) then v85.x=v85.x + ((1378 -(975 + 117)) -(134 + (2026 -(157 + 1718)))) ;elseif (v84==((1353 + 314) -(970 + 695))) then v85.y=v85.y + ((3 -2) -0) ;elseif (v84==(1993 -((1989 -1407) + 1408))) then v85.x=v85.x-(3 -(1020 -(697 + 321))) ;end v86=g_map.getTile(v85);v81=3;end if (v81==0) then v82=g_game.getLocalPlayer();v83=v82:getPosition();v81=1;end if (v81==(7 -4)) then if  not v86 then return;end v87=13431 -(5838 -3082) ;v81=8 -4 ;end if (v81==4) then g_game.useInventoryItemWith(v87,v86:getTopUseThing());break;end if (v81==(1 + 0)) then v84=v82:getDirection();v85={x=v83.x,y=v83.y,z=v83.z};v81=2;end end end);function ativarquebrarTrap() v17.setOn();end function desativarquebrarTrap() v17.setOff();end g_keyboard.bindKeyDown("*",ativarquebrarTrap);g_keyboard.bindKeyUp("*",desativarquebrarTrap);
